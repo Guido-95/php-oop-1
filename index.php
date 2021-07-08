9876538a4adc5b3adfb79b1ac63bbd1298296986
Guido-95 <guidocarna1995@gmail.com>
2021-07-08 16:42:10 +0200

    esercitazione a oggetti

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ class Movie{
     // attributi d'istanza
     public $title;
     public $image;
-    public $weft;
+    public $plot;
     public $vote;
     public $cast;
     public $genre;
@@ -13,10 +13,10 @@ class Movie{
     public $release;
 
     // costruttore
-    function __construct($title,$image, $weft, $vote = 0, $cast, $director = '',$genre = '',$release = '',$duration=''){
+    function __construct($title,$image, $plot, $vote = 0, $cast, $director = '',$genre = '',$release = '',$duration=''){
         $this->title = $title;
         $this->image = $image;
-        $this->weft = $this->subWeft($weft);
+        $this->plot = $this->subPlot($plot);
         $this->cast = $cast;
         $this->director = $director;
         $this->genre = $genre;
@@ -26,25 +26,25 @@ class Movie{
     }
 
     // metodi
-    public function subWeft($weft){
-        return substr($weft,0,110) . '...';
+    public function subPlot($plotMovie){
+        return substr($plotMovie,0,110) . '...';
     }
 }
 
 // film 1
-$weft = "After surviving an unexpected attack in enemy territory, industrialist Tony Stark builds high-tech armor and vows to protect the world as Iron Man.";
+$plot = "After surviving an unexpected attack in enemy territory, industrialist Tony Stark builds high-tech armor and vows to protect the world as Iron Man.";
 $cast = ['Robert Downey jr','Gwyneth Paltrow','Jeff Bridges'];
 $genre = ['Action', 'Science Fiction', 'Adventure'];
 $image='https://image.tmdb.org/t/p/w342/vdb4nVzTy7BXYwF8F99DdszYqOa.jpg';
 
-$film1 = new Movie('Iron man',$image,$weft, 4.5, $cast ,'Jon Favreau', $genre,'01/05/2008','2h 6m');
+$film1 = new Movie('Iron man',$image,$plot, 4.5, $cast ,'Jon Favreau', $genre,'01/05/2008','2h 6m');
 
 // film 2
 $cast = ['Edward Norton','Brad Pitt','Helena Bonham Carter'];
-$weft= 'A ticking-time-bomb insomniac and a slippery soap salesman channel primal male aggression into a shocking new form of therapy. Their concept catches on, with underground "fight clubs" forming in every town, until an eccentric gets in the way and ignites an out-of-control spiral toward oblivion.';
+$plot= 'A ticking-time-bomb insomniac and a slippery soap salesman channel primal male aggression into a shocking new form of therapy. Their concept catches on, with underground "fight clubs" forming in every town, until an eccentric gets in the way and ignites an out-of-control spiral toward oblivion.';
 
 $image2='https://image.tmdb.org/t/p/w342/rtNLQ8HbPElzEfrHjrzSr07prKT.jpg';
-$film2= new Movie('Fight Club', $image2, $weft, 4.6 ,$cast,'David Fincher','Drama','29/10/1999','2h 19m');
+$film2= new Movie('Fight Club', $image2, $plot, 4.6 ,$cast,'David Fincher','Drama','29/10/1999','2h 19m');
 
 ; ?>
 
@@ -72,7 +72,7 @@ $film2= new Movie('Fight Club', $image2, $weft, 4.6 ,$cast,'David Fincher','Dram
             <div>
                 <div class='weft'>
                 
-                <Strong> Plot: </Strong> <?= $film1->weft; ?>
+                <Strong> Plot: </Strong> <?= $film1->plot; ?>
                     
                 </div>
                 
@@ -123,7 +123,7 @@ $film2= new Movie('Fight Club', $image2, $weft, 4.6 ,$cast,'David Fincher','Dram
             </div>
            
             <div class='weft'>
-            <Strong> Plot: </Strong>  <?= $film2->weft; ?>
+            <Strong> Plot: </Strong>  <?= $film2->plot; ?>
                 
             </div>
             <div class="duration-vote">
